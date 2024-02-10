d3cb994c145c18a3203f90b12e4002ec2f9cf659
wbrunovieira <wbrunovieira77@gmail.com>
2024-02-10 11:21:19 -0300

    start header, adde colors, font

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='bg-primary h-32 flex justify-center items-center'>
+      Header
+    </header>
+  );
+};
+
+export default Header;
