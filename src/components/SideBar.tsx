054a12d61197c1b9682360169b8563d89d10ccf7
wbrunovieira <wbrunovieira77@gmail.com>
2024-02-28 15:55:21 -0300

    added sidebar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,21 +1,33 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const Sidebar = () => {
   const sidebarItems = [
-    { icon: 'icon1', title: 'Item 1' },
-    { icon: 'icon2', title: 'Item 2' },
-    { icon: 'icon3', title: 'Item 3' },
-    { icon: 'icon4', title: 'Item 4' },
-    { icon: 'icon5', title: 'Item 5' },
-    { icon: 'icon6', title: 'Item 6' },
+    { src: '/icons/lingerie-mini.svg', title: 'Lingerie' },
+    { src: '/icons/pijamas-mini.svg', title: 'Pijamas' },
+    { src: '/icons/moda-mini.svg', title: 'Moda Fitness' },
+    { src: '/icons/glasses-mini.svg', title: 'Óculos' },
+    { src: '/icons/bag-mini.svg', title: 'Bolsas' },
+    { src: '/icons/perfume-mini.svg', title: 'Perfumes' },
+    { src: '/icons/boy.svg', title: 'Homens' },
   ];
 
   return (
-    <div className='flex flex-col w-64 bg-gray-200'>
+    <div className='flex flex-col w-64 border border-light p-4 mt-2'>
+      <h2 className='text-primaryDark text-base tracking-wider'>Categorias</h2>
       {sidebarItems.map((item, index) => (
-        <div key={index} className='flex items-center p-4'>
-          <div className='mr-2'>{item.icon}</div>
-          <div>{item.title}</div>
+        <div key={index}>
+          <Link href={item.title} className='flex items-center py-4'>
+            <Image
+              src={item.src}
+              width={20}
+              height={20}
+              alt=''
+              className='mr-2'
+            ></Image>
+            <div className='text-xs'>{item.title}</div>
+          </Link>
         </div>
       ))}
     </div>
