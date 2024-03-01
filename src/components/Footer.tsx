16c92a5b22280f8cfe350294b32198ae12a6af84
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-01 09:51:27 -0300

    added border to payments

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -74,64 +74,66 @@ const Footer: React.FC = () => {
             </ul>
           </div>
         </div>
-        <h3 className='text-white-important flex justify-center align-center mt-8'>
-          Pagamento Seguro
-        </h3>
-        <div className='flex gap-2 text-[9px] text-[#787878] justify-center align-center mt-2'>
-          <div className='flex flex-col justify-center items-center'>
-            <MdPix size={20} className='text-white-important' />
-            <p className='mt-1 text-center'>Pix</p>
-          </div>
-          <div className='flex flex-col justify-center items-center'>
-            <Image
-              src={'/icons/debit-card.svg'}
-              alt='cartao de debito'
-              width={20}
-              height={20}
-            />
-            <p className='mt-1 text-center'>Débito</p>
-          </div>
-          <div className='flex flex-col justify-center items-center'>
-            <Image
-              src={'/icons/credit-card.svg'}
-              alt='cartao de debito'
-              width={20}
-              height={20}
-            />
-            <p className='mt-1 text-center'>Crédito</p>
-          </div>
-          <div className='flex flex-col justify-center items-center'>
-            <CiBank size={20} className='text-white-important' />
+        <div className='flex flex-col justify-center align-center border border-light2 rounded p-4 mt-4 w-min mx-auto'>
+          <h3 className='text-white-important flex justify-center align-center  text-sm'>
+            Pagamento Seguro
+          </h3>
+          <div className='flex gap-2 text-[9px] text-[#787878] justify-center align-center mt-2'>
+            <div className='flex flex-col justify-center items-center'>
+              <MdPix size={15} className='text-white-important' />
+              <p className='mt-1 text-center'>Pix</p>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+              <Image
+                src={'/icons/debit-card.svg'}
+                alt='cartao de debito'
+                width={15}
+                height={15}
+              />
+              <p className='mt-1 text-center'>Débito</p>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+              <Image
+                src={'/icons/credit-card.svg'}
+                alt='cartao de debito'
+                width={15}
+                height={15}
+              />
+              <p className='mt-1 text-center'>Crédito</p>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+              <CiBank size={15} className='text-white-important' />
 
-            <p className='mt-1 text-center'>Boleto</p>
-          </div>
-          <div className='flex flex-col justify-center items-center'>
-            <Image
-              src={'/icons/visa.svg'}
-              alt='cartao de debito'
-              width={20}
-              height={20}
-            />
-            <p className='mt-1 text-center'>Visa</p>
-          </div>
-          <div className='flex flex-col justify-center items-center'>
-            <Image
-              src={'/icons/mastercard.svg'}
-              alt='cartao de debito'
-              width={20}
-              height={20}
-              className=''
-            />
-            <p className='mt-1 text-center'>Mastercard</p>
+              <p className='mt-1 text-center'>Boleto</p>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+              <Image
+                src={'/icons/visa.svg'}
+                alt='cartao de debito'
+                width={16}
+                height={16}
+              />
+              <p className='mt-1 text-center'>Visa</p>
+            </div>
+            <div className='flex flex-col justify-center items-center'>
+              <Image
+                src={'/icons/mastercard.svg'}
+                alt='cartao de debito'
+                width={15}
+                height={15}
+                className='flex flex-col justify-center items-center'
+              />
+              <p className='mt-1 text-center'>Mastercard</p>
+            </div>
           </div>
         </div>
       </div>
       <a
-        className='text-[#ebe8e8] text-xs flex justify-center align-center pb-2'
+        className='text-[#ebe8e8] text-xs flex justify-center align-center '
         href='https://www.wbdigitalsolutions.com/'
         target='_blank'
       >
-        <p>Copyright © WB Digital Solutions</p>
+        <p className='text-[9px] mt-4'>Copyright © WB Digital Solutions</p>
       </a>
     </footer>
   );
