69e86b73d3195af4a3224a4f1c13b8e642ce95a0
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-08 12:03:19 -0300

    close the float cart

--- src/components/FloatCart.tsx
+++ src/components/FloatCart.tsx
@@ -7,9 +7,27 @@ interface FloatCartProps {
 }
 
 const FloatCart: React.FC<FloatCartProps> = ({ onClose }) => {
+  const handleClickOutside = (event: React.MouseEvent) => {
+    if ((event.target as HTMLElement).classList.contains('modal-background')) {
+      onClose();
+    }
+  };
   return (
-    <div className='fixed top-28 right-2 flex items-center justify-center z-50 bg-primaryLight border rounded-md shadow-xl border-secondary bg-opacity-50'>
-      <div className='bg-white rounded-lg p-6 mb-4 '>
+    <div
+      className='fixed top-28 right-2 flex items-center justify-center z-50 bg-primaryLight border rounded-md shadow-xl border-secondary bg-opacity-50'
+      onClick={handleClickOutside}
+    >
+      <div
+        className='relative bg-white rounded-lg shadow-xl p-6 mb-4 max-w-lg w-full'
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          onClick={onClose}
+          className='text-xl text-primaryDark font-semibold'
+        >
+          X
+        </button>
+
         <div className='flex justify-center items-center gap-2  border-b-2 border-light pb-4'>
           <h2 className='text-xl font-bold'>Carrinho - </h2>
           <span className='fontbold text-xl'>4 itens</span>
@@ -29,19 +47,7 @@ const FloatCart: React.FC<FloatCartProps> = ({ onClose }) => {
               <p className='text-xs text-gray-500'>R$ 100,00</p>
             </div>
           </div>
-          <div className='flex items-center mb-4'>
-            <Image
-              src='/images/liz1.webp'
-              alt='Item 1'
-              width={24}
-              height={24}
-              className='w-12 h-12 rounded mr-4'
-            />
-            <div className='flex justify-center items-center gap-2'>
-              <p className='text-xs font-medium'>Calcinha Biquini 50030</p>
-              <p className='text-xs text-gray-500'>R$ 100,00</p>
-            </div>
-          </div>
+
           <div className='flex items-center mb-4'>
             <Image
               src='/images/liz1.webp'
