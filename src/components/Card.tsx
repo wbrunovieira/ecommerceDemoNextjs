d26f33162a70bf3d38acd2f0aef15c10fcbbbc0a
wbrunovieira <wbrunovieira77@gmail.com>
2024-03-09 10:55:36 -0300

    fixed card, button position when had the descont

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -24,10 +24,10 @@ const Card: React.FC<CardProps> = ({
   eNovidade,
 }) => {
   return (
-    <div className='max-w-sm rounded  shadow-lg bg-white m-2 h-96 '>
-      <div className='relative overflow-hidden'>
+    <div className='max-w-sm rounded shadow-lg bg-white m-2 h-96 flex flex-col'>
+      <div className='relative overflow-hidden flex-shrink-0'>
         <Image
-          className='w-full relative'
+          className='w-full'
           src={imageSRC}
           width={400}
           height={300}
@@ -37,14 +37,20 @@ const Card: React.FC<CardProps> = ({
           <div className='absolute promocao1 transform rotate-45 translate-x-1/3 -translate-y-1/3 px-2 py-1 text-xs uppercase'>
             PROMOÇÃO
           </div>
-        )}{' '}
+        )}
         {eNovidade && (
           <div className='absolute novidade transform rotate-45 translate-x-1/3 -translate-y-1/3 px-2 py-1 text-xs uppercase'>
             Novidade
           </div>
         )}
-        {desconto && <div className='descont-badge'>{`${desconto}%`}</div>}
-        <div className='px-6 py-4'>
+        {desconto && (
+          <div className='relative'>
+            <div className='absolute descont-badge'>{`${desconto}%`}</div>
+          </div>
+        )}
+      </div>
+      <div className='flex-1 px-6 py-4 flex flex-col justify-between'>
+        <div>
           <h3 className='font-regular text-xs text-primary'>{category}</h3>
           <h2 className='semibold text-xl mb-2 text-fontColor'>{title}</h2>
           <p className='text-gray-700 text-sm font-bold text-fontColor'>
@@ -53,10 +59,10 @@ const Card: React.FC<CardProps> = ({
                 R${precoAntigo.toFixed(2)}
               </span>
             )}
-            <span className='text-red-500'> R${precoNovo.toFixed(2)}</span>
+            <span className='text-red-500'>R${precoNovo.toFixed(2)}</span>
           </p>
         </div>
-        <div className='px-6 pt-4 pb-2'>
+        <div className='pb-2'>
           <Button variant='secondary' size='small'>
             Comprar
           </Button>
