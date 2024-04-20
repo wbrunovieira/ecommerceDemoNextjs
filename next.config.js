a59475d5ad20698250d3ff6a12a5fe30326dfae7
wbrunovieira <wbrunovieira77@gmail.com>
2024-04-20 11:04:06 -0300

    styled the loggin button and added user image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 // next.config.js
 const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com'],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
--- src/components/LogginButton.tsx
+++ src/components/LogginButton.tsx
@@ -1,23 +1,41 @@
 'use client';
 import { useSession, signIn, signOut } from 'next-auth/react';
+import { FaUserAlt } from 'react-icons/fa';
+import Image from 'next/image';
 import Link from 'next/link';
 
 const LogginButton = () => {
   const { data: session, status } = useSession();
 
   if (status === 'loading') {
-    return <div>Carregando...</div>;
+    return <div className='text-xs'>Carregando...</div>;
   }
 
   if (session) {
     return (
-      <div>
+      <div className='flex flex-col items-center '>
         <Link href={`/user/${session.id}`} passHref>
-          <span className='text-secondary transition duration-300 hover:scale-110'>
+          <div className=' flex text-secondary transition duration-300 hover:scale-110 text-xs font-semibold border p-2 rounded-md bg-primaryLight items-center'>
+            {session.image ? (
+              <Image
+                src={session.image}
+                alt='Imagem do usuário'
+                width={12}
+                height={12}
+                className='w-6 h-6 rounded-full items-center mr-2 border border-secondary '
+              />
+            ) : (
+              <FaUserAlt className='w-6 h-6 text-gray-400' />
+            )}
             Olá, {session.name}
-          </span>
+          </div>
         </Link>
-        <button onClick={() => signOut()}>Sair</button>
+        <button
+          className='hover:scale-110 text-xs mt-1 mb-1 bg-primaryLight p-1 rounded-md'
+          onClick={() => signOut()}
+        >
+          Sair
+        </button>
       </div>
     );
   } else {
