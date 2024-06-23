4e1023c471de40b779a9f6850de5a4126d24da30
bruno-minimac <wbrunovieira77@gmail.com>
2024-06-23 09:54:33 -0300

    fixed render in cart page

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,8 +1,9 @@
-'use client';
-import { useCartStore } from '@/context/store';
-import Image from 'next/image';
-import Link from 'next/link';
-import { BsTrash, BsPlus, BsDash } from 'react-icons/bs';
+"use client";
+import { useCartStore } from "@/context/store";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect } from "react";
+import { BsTrash, BsPlus, BsDash } from "react-icons/bs";
 interface Product {
   id: string;
   quantity: number;
@@ -12,8 +13,15 @@ interface Product {
 }
 const Cart = () => {
   const cartItems = useCartStore((state: any) => state.cartItems);
-  const updateQuantity = useCartStore((state: any) => state.updateQuantity);
+  const addToCart = useCartStore((state: any) => state.addToCart);
   const removeFromCart = useCartStore((state: any) => state.removeFromCart);
+  const updateQuantity = useCartStore((state: any) => state.updateQuantity);
+
+  useEffect(() => {
+    addToCart(cartItems);
+  }, []);
+
+  console.log("cartItems", cartItems);
   const increaseQuantity = (id: string) => {
     updateQuantity(id, 1);
   };
@@ -22,75 +30,75 @@ const Cart = () => {
     updateQuantity(id, -1);
   };
   return (
-    <div className='bg-primaryLight text-fontColor w-[600px] mt-4 p-8 rounded-lg'>
-      <div className='container mx-auto py-4 '>
-        <h1 className='text-3xl font-bold mb-4 text-fontColor '></h1>
+    <div className="bg-primaryLight text-fontColor w-[600px] mt-4 p-8 rounded-lg">
+      <div className="container mx-auto py-4 ">
+        <h1 className="text-3xl font-bold mb-4 text-fontColor "></h1>
 
-        <div className='flex flex-col gap-4 w-[450px] bg-primary rounded-md'>
+        <div className="flex flex-col gap-4 w-[450px] bg-primary rounded-md">
           {cartItems.map((item: Product, index: number) => (
             <div
               key={index}
-              className='bg-white p-4 rounded shadow flex gap-4 '
+              className="bg-white p-4 rounded shadow flex gap-4 "
             >
               <Image
-                className='rounded-xl'
-                src='/images/liz1.webp'
+                className="rounded-xl"
+                src="/images/liz1.webp"
                 width={50}
                 height={50}
-                alt=''
+                alt=""
               />
 
-              <div className='flex flex-col justify-center items-center'>
-                <h2 className='text-base font-semibold text-primaryDark '>
-                  QTD{' '}
+              <div className="flex flex-col justify-center items-center">
+                <h2 className="text-base font-semibold text-primaryDark ">
+                  QTD{" "}
                 </h2>
-                <div className='flex justify-center items-center'>
+                <div className="flex justify-center items-center">
                   <button
                     onClick={() => decreaseQuantity(item.id)}
-                    className='text-sm p-1'
+                    className="text-sm p-1"
                   >
                     <BsDash />
                   </button>
-                  <h2 className='text-base font-light mb-2'>
-                    {' '}
+                  <h2 className="text-base font-light mb-2">
+                    {" "}
                     {item.quantity}
                   </h2>
                   <button
                     onClick={() => increaseQuantity(item.id)}
-                    className='text-sm p-1'
+                    className="text-sm p-1"
                   >
                     <BsPlus />
                   </button>
                 </div>
               </div>
-              <div className='flex flex-col justify-center items-center'>
-                <h2 className='text-base font-semibold text-primaryDark '>
-                  Produto{' '}
+              <div className="flex flex-col justify-center items-center">
+                <h2 className="text-base font-semibold text-primaryDark ">
+                  Produto{" "}
                 </h2>
-                <h2 className='text-base font-light mb-2 whitespace-nowrap'>
+                <h2 className="text-base font-light mb-2 whitespace-nowrap">
                   {item.title}
                 </h2>
               </div>
-              <div className='flex flex-col justify-center items-center'>
-                <p className='text-base font-semibold text-primaryDark '>
-                  Preço{' '}
+              <div className="flex flex-col justify-center items-center">
+                <p className="text-base font-semibold text-primaryDark ">
+                  Preço{" "}
                 </p>
-                <p className='text-base font-light mb-2 whitespace-nowrap'>
-                  {item.price.toFixed(2)}
-                </p>
-              </div>
-              <div className='flex flex-col justify-center items-center'>
-                <p className='text-base font-semibold text-primaryDark '>
-                  Total{' '}
-                </p>
-                <p className='text-base font-light mb-2 whitespace-nowrap'>
-                  {(item.price * item.quantity).toFixed(2)}
+                <p className="text-base font-light mb-2 whitespace-nowrap">
+                  {item.price !== undefined ? item.price.toFixed(2) : "0.00"}
                 </p>
               </div>
-              <div className='flex flex-col justify-center items-center ml-4'>
+              <div className="flex flex-col justify-center items-center">
+                <p className="text-base font-semibold text-primaryDark ">
+                  Total{" "}
+                </p>
+                <p className="text-base font-light mb-2 whitespace-nowrap">
+                  {item.price !== undefined ? item.price.toFixed(2) : "0.00"}
+                </p>
+              </div>
+              <div className="flex flex-col justify-center items-center ml-4">
                 <button
                   onClick={() => removeFromCart(item.id)}
-                  className='bg-secondary text-white-important py-2 text-xs px-4 rounded transition duration-300 hover:scale-105'
+                  className="bg-secondary text-white-important py-2 text-xs px-4 rounded transition duration-300 hover:scale-105"
                 >
                   <BsTrash />
                 </button>
@@ -99,28 +107,29 @@ const Cart = () => {
           ))}
         </div>
 
-        <div className='mt-8'>
-          <h2 className='text-2xl font-semibold mb-2'>
-            Total: R${' '}
+        <div className="mt-8">
+          <h2 className="text-2xl font-semibold mb-2">
+            Total: R${" "}
             {cartItems
               .reduce(
                 (acc: number, item: Product) =>
-                  acc + item.price * item.quantity,
+                  acc +
+                  (item.price !== undefined ? item.price * item.quantity : 0),
                 0
               )
               .toFixed(2)}
           </h2>
-          <div className='flex justify-between items-center'>
-            <Link href='/'>
-              <div className='bg-secondary text-primaryLight py-2 px-4 rounded transition duration-300 hover:scale-110'>
+          <div className="flex justify-between items-center">
+            <Link href="/">
+              <div className="bg-secondary text-primaryLight py-2 px-4 rounded transition duration-300 hover:scale-110">
                 Adcionar mais produtos
               </div>
             </Link>
             <Link
-              href='/cart'
-              className='px-4 py-2 mt-2 rounded bg-secondary text-primaryLight transition duration-300 hover:scale-110'
+              href="/cart"
+              className="px-4 py-2 mt-2 rounded bg-secondary text-primaryLight transition duration-300 hover:scale-110"
             >
-              <div className=''>Finalizar</div>
+              <div className="">Finalizar</div>
             </Link>
           </div>
         </div>
