795c5ecdf940d808e9477e694b81aefd19b68f22
bruno-minimac <wbrunovieira77@gmail.com>
2024-07-05 18:57:20 -0300

    lsit categories and edi in adm page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -35,19 +35,35 @@ interface Size {
   };
 }
 
+interface Category {
+  _id: {
+    value: string;
+  };
+  props: {
+    name: string;
+    imageUrl: string;
+    erpId: string;
+  };
+}
+
 const AdminPage: React.FC = () => {
   const router = useRouter();
 
   const { data: session, status } = useSession();
   const [colors, setColors] = useState<Color[]>([]);
   const [sizes, setSizes] = useState<Size[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
 
   const [editingColorId, setEditingColorId] = useState<string | null>(null);
   const [editColorData, setEditColorData] = useState({ name: "", hex: "" });
   const [editingSizeId, setEditingSizeId] = useState<string | null>(null);
   const [editSizeData, setEditSizeData] = useState({ name: "", description: "" });
+  const [editingCategoryId, setEditingCategoryId] = useState<string | null>(null);
+  const [editCategoryData, setEditCategoryData] = useState({ name: "", imageUrl: "" });
+ 
   console.log('colors',colors)
   console.log('sizes',sizes)
+  console.log('categories',categories)
   
 
   useEffect(() => {
@@ -93,8 +109,23 @@ const AdminPage: React.FC = () => {
       }
     };
 
+      const fetchCategories = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3333/category/all?page=1&pageSize=50`, {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+        console.log('response.data.categories',response.data.categories)
+        setCategories(response.data.categories);
+      } catch (error) {
+        console.error("Erro ao buscar as categorias: ", error);
+      }
+    };
+
     fetchColors();
     fetchSizes();
+    fetchCategories()
   }, []);
 
   const handleEditClick = (color: Color) => {
@@ -159,6 +190,37 @@ const AdminPage: React.FC = () => {
     }
   }
 
+  const handleEditCategoryClick = (category: Category) => {
+    setEditingCategoryId(category._id.value);
+    setEditCategoryData({ name: category.props.name, imageUrl: category.props.imageUrl });
+  };
+
+  const handleCategoryInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setEditCategoryData(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  const handleSaveCategoryClick = async (categoryId: string) => {
+    try {
+      await axios.put(
+        `http://localhost:3333/category/${categoryId}`,
+        { name: editCategoryData.name, imageUrl: editCategoryData.imageUrl},
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${session?.accessToken}`
+          }
+        }
+      );
+      setCategories(prevCategories => prevCategories.map(category => 
+        category._id.value === categoryId ? { ...category, props: { ...category.props, ...editCategoryData } } : category
+      ));
+      setEditingCategoryId(null);
+    } catch (error) {
+      console.error("Erro ao salvar a categoria: ", error);
+    }
+  };
+
   if (status === "loading") {
     return <div>Carregando...</div>;
   }
@@ -195,20 +257,79 @@ const AdminPage: React.FC = () => {
           <div className="bg-primaryLight dark:bg-primaryDark  p-2 rounded">
             <Sheet>
               <SheetTrigger>Categoria</SheetTrigger>
-              <SheetContent
-                side="right"
-                size="extraLarge"
-               
-              >
+              <SheetContent side="right" size="extraLarge">
                 <SheetHeader>
                   <SheetTitle>Categoria</SheetTitle>
-                  <SheetDescription>
-                    Categoria Descricao
-                  </SheetDescription>
+                  <SheetDescription>Categoria Descrição</SheetDescription>
                 </SheetHeader>
+                <div className="p-4">
+                  <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                    <thead className="bg-primaryLight dark:bg-primaryDark rounded">
+                      <tr>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-primaryDark dark:text-primaryLight uppercase tracking-wider">ID</th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-primaryDark dark:text-primaryLight uppercase tracking-wider">Nome</th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-primaryDark dark:text-primaryLight uppercase tracking-wider">imagem</th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-primaryDark dark:text-primaryLight uppercase tracking-wider">ERP ID</th>
+                        <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-primaryDark dark:text-primaryLight uppercase tracking-wider">Ações</th>
+                      </tr>
+                    </thead>
+                    <tbody className="bg-primaryLight dark:bg-primaryDark divide-y divide-gray-200 dark:divide-gray-700">
+                      {categories.map((category) => (
+                        <tr key={category._id.value}>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-200">{category._id.value}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-200">
+                            {editingCategoryId === category._id.value ? (
+                              <input
+                                type="text"
+                                name="name"
+                                value={editCategoryData.name}
+                                onChange={handleCategoryInputChange}
+                                className="px-2 py-1 border border-gray-300 rounded"
+                              />
+                            ) : (
+                              category.props.name
+                            )}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-200">
+                            {editingCategoryId === category._id.value ? (
+                              <input
+                                type="text"
+                                name="description"
+                                value={editCategoryData.imageUrl}
+                                onChange={handleCategoryInputChange}
+                                className="px-2 py-1 border border-gray-300 rounded"
+                              />
+                            ) : (
+                              category.props.imageUrl
+                            )}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-200">{category.props.erpId}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-200">
+                            {editingCategoryId === category._id.value ? (
+                              <button
+                                onClick={() => handleSaveCategoryClick(category._id.value)}
+                                className="px-4 py-2 bg-secondary text-white rounded"
+                              >
+                                Salvar
+                              </button>
+                            ) : (
+                              <button
+                                onClick={() => handleEditCategoryClick(category)}
+                                className="px-4 py-2 bg-primary text-white rounded"
+                              >
+                                Editar
+                              </button>
+                            )}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
               </SheetContent>
             </Sheet>
           </div>
+          
 
 
    <div className="bg-primaryLight dark:bg-primaryDark p-2 rounded">
