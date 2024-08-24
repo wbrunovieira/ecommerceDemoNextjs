1498c6de9983f9ae6d90a1f065c789ffec0b9558
bruno-minimac <wbrunovieira77@gmail.com>
2024-08-24 10:16:33 -0300

    reset-password page, improved layout and became responsive

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -37,9 +37,9 @@ const ResetPassword = () => {
     function getMessageForField(field: keyof ErrorMessages): string {
         switch (field) {
             case 'newPassword':
-                return 'Vamos precisar do sua nova senha. :(';
+                return 'Por favor, insira sua nova senha.';
             case 'repeatPassword':
-                return 'Você precisa repetir a senha.';
+                return 'Você precisa repetir a senha para confirmá-la.';
 
             default:
                 return '';
@@ -65,12 +65,13 @@ const ResetPassword = () => {
                     },
                 }
             );
-            console.log(' result',result)
+            console.log(' result', result);
 
             if (result.status === 201) {
                 toast({
-                    title: 'Sucesso',
-                    description: 'Senha redefinida com sucesso!',
+                    title: 'Senha redefinida com sucesso!',
+                    description:
+                        'Sua nova senha foi configurada. Agora você pode fazer login com ela.',
                 });
                 router.push('/login');
             }
@@ -106,7 +107,8 @@ const ResetPassword = () => {
             }
 
             if (field === 'repeatPassword' && value !== newPassword) {
-                errorMessage = 'As senhas não coincidem.';
+                errorMessage =
+                    'As senhas não coincidem.Por favor, verifique e tente novamente.';
             }
 
             return {
@@ -117,44 +119,54 @@ const ResetPassword = () => {
     };
 
     return (
-        <div className="fixed inset-0 flex items-center justify-center">
-            <div className="absolute bg-primaryLight dark:bg-dark-secondary-gradient p-96 rounded-lg shadow-lg z-10 relative overflow-hidden lg:p-96 md:p-48 sm:w-full">
-                <div>Digite a nova senha</div>
+        <div className="flex md:min-h-screen items-center justify-center  bg-linear-gradient  p-4 sm:p-6 lg:p-8">
+            <div className="bg-white dark:bg-dark-secondary-gradient p-6 sm:p-8 lg:p-10 border-y-primaryDark rounded-lg shadow-lg z-10 relative overflow-hidden w-full md:w-3/5">
+                <h2 className="text-xl font-semibold text-gray-800 dark:text-white mb-4">
+                    Vamos redefinir sua senha
+                </h2>
+                <p className="text-sm text-gray-600 dark:text-gray-300">
+                    Por favor, insira sua nova senha e confirme-a abaixo.
+                </p>
+                <p className="text-sm text-gray-600 dark:text-gray-300 mb-6">
+                    Não se preocupe, estamos aqui para garantir que sua nova
+                    senha seja segura e fácil de lembrar.
+                </p>
+
                 <form
                     onSubmit={handleSubmit}
-                    className="flex flex-col space-y-4"
+                    className="flex flex-col space-y-6"
                 >
-                    <label
-                        htmlFor="email"
-                        className="text-primaryDark dark:text-white-important text-xs"
-                    >
-                        Senha
-                    </label>
-                    <div className="relative w-96 md:w-72 sm:w-32">
-                        <input
-                            id="password"
-                            type={showPassword ? 'text' : 'password'}
-                            name="password"
-                            required
-                            autoComplete="password"
-                            placeholder="Senha"
-                            onBlur={() =>
-                                handleBlur('newPassword', newPassword)
-                            }
-                            value={newPassword}
-                            onChange={(e) => setnewPassword(e.target.value)}
-                            className="px-4 py-2 rounded-lg shadow-sm bg-white bg-opacity-80 w-full pr-10 focus:border-primaryDark focus:ring-2 focus:ring-secondary focus:ring-opacity-50 focus:outline-none caret-secondary placeholder-primary"
-                        />
+                    <div className="relative w-full">
+                        <label
+                            htmlFor="email"
+                            className="text-primaryDark dark:text-white-important text-xs"
+                        >
+                            Senha
+                            <input
+                                id="password"
+                                type={showPassword ? 'text' : 'password'}
+                                name="password"
+                                required
+                                autoComplete="password"
+                                placeholder="Senha"
+                                onBlur={() =>
+                                    handleBlur('newPassword', newPassword)
+                                }
+                                value={newPassword}
+                                onChange={(e) => setnewPassword(e.target.value)}
+                                className="mt-1 px-4 py-2 pr-10 rounded-lg border border-gray-300 shadow-sm bg-white bg-opacity-80 dark:border-gray-700 w-full max-w-full focus:ring-secondary focus:border-secondary dark:bg-gray-700 dark:text-gray-300 text-sm"
+                            />
+                        </label>
                         <span className="absolute inset-y-0 right-0 flex items-center pr-3">
                             {showPassword ? (
                                 <FiEyeOff
                                     onClick={togglePasswordVisibility}
-                                    className="cursor-pointer text-gray-500"
+                                    className="cursor-pointer mt-6 text-gray-500"
                                 />
                             ) : (
                                 <FiEye
                                     onClick={togglePasswordVisibility}
-                                    className="cursor-pointer text-gray-500"
+                                    className="cursor-pointer mt-6 text-gray-500"
                                 />
                             )}
                         </span>
@@ -165,13 +177,13 @@ const ResetPassword = () => {
                         )}
                     </div>
 
-                    <label
-                        htmlFor="email"
-                        className="text-primaryDark dark:text-white-important text-xs"
-                    >
-                        Repetir a senha
-                    </label>
-                    <div className="relative w-96 md:w-72 sm:w-32">
+                    <div className="relative w-full">
+                        <label
+                            htmlFor="email"
+                            className="text-primaryDark dark:text-white-important text-xs"
+                        >
+                            Repetir a senha
+                        </label>
                         <input
                             id="repeatPassword"
                             type={showPassword ? 'text' : 'password'}
@@ -183,18 +195,18 @@ const ResetPassword = () => {
                             }
                             required
                             placeholder="Repetir a senha"
-                            className="px-4 py-2 rounded-lg shadow-sm bg-white bg-opacity-80 w-96 md:w-72 sm:w-32 mb-4 focus:border-primaryDark focus:ring-2 focus:ring-secondary focus:ring-opacity-50 focus:outline-none caret-secondary placeholder-primary"
+                            className="mt-1 px-4 py-2 pr-10 rounded-lg border border-gray-300 shadow-sm bg-white bg-opacity-80 dark:border-gray-700 w-full max-w-full focus:ring-secondary focus:border-secondary dark:bg-gray-700 dark:text-gray-300 text-sm"
                         />
                         <span className="absolute inset-y-0 right-0 flex items-center pr-3">
                             {showPassword ? (
                                 <FiEyeOff
                                     onClick={togglePasswordVisibility}
-                                    className="cursor-pointer text-gray-500 "
+                                    className="cursor-pointer mt-6 text-gray-500 "
                                 />
                             ) : (
                                 <FiEye
                                     onClick={togglePasswordVisibility}
-                                    className="cursor-pointer text-gray-500"
+                                    className="cursor-pointer mt-6 text-gray-500"
                                 />
                             )}
                         </span>
@@ -205,7 +217,7 @@ const ResetPassword = () => {
                         </p>
                     )}
                     {errorMessage.repeatPassword && (
-                        <p className="text-primaryDark w-48 rounded-md text-xs mb-4 bg-primaryLight p-2">
+                        <p className="text-redAtention text-xs italic">
                             {errorMessage.repeatPassword}
                         </p>
                     )}
@@ -213,7 +225,7 @@ const ResetPassword = () => {
                     <button
                         type="submit"
                         disabled={isButtonInDisabled}
-                        className="flex items-center justify-center bg-secondary text-white-important px-4 py-2 rounded-lg shadow-md hover:bg-secondary-dark w-96 md:w-72 sm:w-32 transition duration-300 hover:scale-105"
+                        className="w-full flex items-center justify-center bg-secondary text-white-important px-4 py-2 rounded-lg shadow-md hover:bg-secondary-dark w-full max-w-full transition duration-300 hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-secondary-dark"
                     >
                         {isButtonInDisabled ? (
                             <svg
@@ -221,23 +233,23 @@ const ResetPassword = () => {
                                 viewBox="0 0 24 24"
                             ></svg>
                         ) : null}
-                        Enviar
+                        Enviar nova senha
                     </button>
-
-                    <Link href="/login">
-                        <button
-                            type="button"
-                            className="bg-secondary text-white-important px-2  text-xs py-1 rounded-lg shadow-md hover:bg-secondary-dark w-96 md:w-72 sm:w-32 transition duration-300 hover:scale-105"
+                    <div className="flex justify-between items-center mt-4 gap-2">
+                        <Link
+                            href="/login"
+                            className="bg-secondary text-white-important px-4 text-xs py-2 rounded-lg shadow-md hover:bg-secondary-dark w-full max-w-full transition duration-300 hover:scale-105 whitespace-nowrap"
                         >
-                            Voltar para Login
-                        </button>
-                    </Link>
+                            Voltar para o Login
+                        </Link>
 
-                    <Link href="/" legacyBehavior>
-                        <a className="bg-secondary text-white-important px-4 py-2 rounded-lg shadow-md hover:bg-secondary-dark w-32 md:w-32 sm:w-32 transition duration-300 hover:scale-105 mt-8">
-                            Home
-                        </a>
-                    </Link>
+                        <Link
+                            href="/"
+                            className="bg-secondary text-center text-white-important px-4 py-2 text-xs rounded-lg shadow-md hover:bg-secondary-dark w-full max-w-full transition duration-300 hover:scale-105 whitespace-nowrap"
+                        >
+                            Voltar para a Home
+                        </Link>
+                    </div>
                 </form>
             </div>
         </div>
