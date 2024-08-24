fe96c1e9ddffd7fdcf07140fc2617e0b3ce8777f
bruno-minimac <wbrunovieira77@gmail.com>
2024-08-24 11:34:16 -0300

    pagamento pendente improved layout and responsive

--- src/app/pagamento-falhou/page.tsx
+++ src/app/pagamento-falhou/page.tsx
@@ -6,8 +6,8 @@ const PaymentFayout = () => {
     const preferenceId = '<PREFERENCE_ID>';
     return (
         <div className="bg-linear-gradient flex items-center justify-center md:min-h-screen py-8">
-            <div className="flex flex-col items-center justify-center z-10 bg-white p-8 border-2 border-y-primaryDark rounded-lg shadow-lg  md:w-auto w-full md-w-3/5">
-                <div className="flex flex-col  mb-2 w-full ">
+            <div className="flex flex-col items-center justify-center z-10 bg-white p-8 border-2 border-y-primaryDark rounded-lg shadow-lg  md:w-auto w-full md-w-3/5 ">
+                <div className="flex flex-col  mb-2 w-full  ">
                     <h1 className="text-lg md:text-3xl text-primaryDark font-bold text-center animate-fade-in whitespace-nowrap ">
                         Algo deu errado com o pagamento
                     </h1>
--- src/app/pagamento-pendente/page.tsx
+++ src/app/pagamento-pendente/page.tsx
@@ -13,12 +13,26 @@ const ShippingOptions = () => {
     const { cartItems, selectedAddress } = useCartStore();
 
     const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL_BACKEND;
-    const preferenceId = '<PREFERENCE_ID>'; 
+    const preferenceId = '<PREFERENCE_ID>';
     return (
-        <div className="fixed inset-0 flex items-center justify-center">
-            <div className="bg-primaryLight p-8 rounded-lg shadow-lg z-10 relative overflow-hidden lg:p-16 md:p-12 sm:w-full">
-                <div className="relative z-10 bg-primary p-8 border-2 border-y-primaryDark rounded-lg shadow-lg">
-                <h1 className="text-3xl font-bold text-center my-8">Pagamento Pendente</h1>
+        <div className="bg-linear-gradient flex items-center justify-center md:min-h-screen mt-8">
+            <div className="flex flex-col items-center justify-center z-10 bg-white p-8 border-2 border-y-primaryDark rounded-lg shadow-lg max-w-lg w-full md:w-auto mt-8">
+                <div className="flex flex-col mb-2 w-full">
+                    <h1 className="text-lg md:text-3xl text-primaryDark font-bold text-center animate-fade-in">
+                        Pagamento Pendente
+                    </h1>
+                    <p className="text-sm md:text-lg text-gray-600 text-left mt-4">
+                        Seu pagamento ainda está sendo processado.
+                    </p>
+                    <p className="text-sm md:text-lg text-gray-600 text-left ">
+                        Não se preocupe, você será notificado assim que o
+                        pagamento for confirmado.
+                    </p>
+                    <p className="text-sm md:text-lg text-gray-600 text-left mb-4">
+                        Enquanto isso, você pode revisar seus detalhes de compra
+                        ou escolher outro método de pagamento se desejar.
+                    </p>
+                    <hr className="border-0 h-[2px] bg-gradient-to-r from-primary to-primary-light mt-1 w-full mb-6" />
                     <WalletComponent preferenceId={preferenceId} />
                 </div>
             </div>
