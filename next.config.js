105da43688d994d82d7c25c454dfd9066b19fc52
Bruno-macminiUbuntu <wbrunovieira77@gmail.com>
2024-12-03 10:37:56 -0300

    Corrige importação de globals.css e configurações

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+
 const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com', 'cdn.connectplug.com.br'],
@@ -13,10 +14,9 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: '/(.*).(js|css|png|jpg|svg)',
+                source: '/_next/static/:path*',
                 headers: [
                     { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
-                    { key: 'Content-Type', value: 'application/javascript' }, 
                 ],
             },
         ];
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 
-import '../globals.css';
+import './globals.css';
 
 import { Montserrat } from 'next/font/google';
 import { Toaster } from '@/components/ui/toaster';
